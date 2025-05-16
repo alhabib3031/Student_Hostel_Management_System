c8df6d53107fffa88271cfded0c0ca976d0fbb1c
alhabib ahmed <itstd.5377@uob.edu.ly>
2025-05-16 16:15:19 +0200

    تم تغيير معظم الصفحات باللغة العربية واتجاه الصفحات كذلك إضافة صفحة تقديم التسجيل ولكن غير مكتملة اضافة فئات مثل الادمن وصفحات للأدمن

---
+++ Student_Hostel_Management_System/Data/Entities/Administration_Manager.cs
@@ -0,0 +1,12 @@
+﻿namespace Student_Hostel_Management_System.Data.Entities
+{
+    public class Administration_Manager
+    {
+        public int Id { get; set; }
+        public int EID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+    }
+}
--- Student_Hostel_Management_System/Data/Entities/Student.cs
+++ Student_Hostel_Management_System/Data/Entities/Student.cs
@@ -10,7 +10,7 @@ namespace Student_Hostel_Management_System.Data.Entities
         public string PhoneNumber { get; set; }
         public string Address { get; set; }
         public string CollegeName { get; set; }
-        public enStatus Status { get; set; }
+        public enStatus Status { get; set; } // نظامي ام غير نظامي ام موقوف ام متخرج ام مفصول
 
     }
 }
