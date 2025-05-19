a6a47f185bfb2b539be1e5782fe8b10e9a39f802
alhabib ahmed <itstd.5377@uob.edu.ly>
2025-05-20 01:05:17 +0200

    تم إنشاء فئة الأدمن والطالب وعمل علاقة واحد إلي متعدد بينهما

---
+++ Student_Hostel_Management_System/Data/Entites/Administration.cs
@@ -0,0 +1,12 @@
+﻿namespace Student_Hostel_Management_System.Data.Entites
+{
+    public class Administration
+    {
+        public Guid Id { get; set; }
+        public int NID { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public List<Student> Students { get; set; } = [];
+    }
+}
---
+++ Student_Hostel_Management_System/Data/Entites/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace Student_Hostel_Management_System.Data.Entites
+{
+    public class Student
+    {
+        public Guid Id { get; set; }
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public Guid AdministrationId { get; set; }
+        public Administration Administration { get; set; }
+    }
+}
